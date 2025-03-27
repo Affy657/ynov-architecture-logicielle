c7ba53be20f46f8d457833ff3c1232ce32214aac
eliot guihal <eliot.guihal@ynov.com>
2025-03-27 13:52:16 +0000

    Vite config bis

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,7 @@
 import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
-import { resolve } from 'path';
 
 export default defineConfig({
-  plugins: [tsconfigPaths()],
   server: {
-    port: process.env.FRONT_PORT as unknown as number,
-  },
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'src/modules/MissionControl/index.html'),
-      },
-    },
+    port: Number(process.env.SERVER_PORT),
   },
 });
